70595d658db308798d4510c4595b48111bf02365
agaHuseynoff <huseynov_aga@list.ru>
2019-07-17 00:06:20 +0400

    new

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,100 +1,115 @@
-import { rerenderEntireTree } from "../render";
-
-
-let state = {
-    profilePage: {
-        posts: [
-            {
-                id: 1,
-                message: 'Hi , how are you?',
-                likesCount: 12,
-                img: 'http://www.luljettas.com/images/avatar/img-6.jpg'
-            }, {
-                id: 2,
-                message: 'It\'s my first post.',
-                likesCount: 7,
-                img: 'https://st2.depositphotos.com/1007566/12304/v/950/depositphotos_123041444-stock-' +
-                        'illustration-avatar-man-cartoon.jpg'
-            }, {
-                id: 3,
-                message: 'Blabla',
-                likesCount: 7,
-                img: 'https://st4.depositphotos.com/5934840/25751/v/450/depositphotos_257517000-stock-' +
-                        'illustration-man-portrait-avatar.jpg'
-            }, {
-                id: 4,
-                message: 'da da',
-                likesCount: 7,
-                img: 'http://www.avatars24.de/img/avatars/avatar-5.jpg'
-            }
-        ],
-
-        newPostText: 'agahuseynoff.com'
+let store = {
+     _state: {
+        profilePage: {
+            posts: [
+                {
+                    id: 1,
+                    message: 'Hi , how are you?',
+                    likesCount: 12,
+                    img: 'http://www.luljettas.com/images/avatar/img-6.jpg'
+                }, {
+                    id: 2,
+                    message: 'It\'s my first post.',
+                    likesCount: 7,
+                    img: 'https://st2.depositphotos.com/1007566/12304/v/950/depositphotos_123041444-stock-' +
+                            'illustration-avatar-man-cartoon.jpg'
+                }, {
+                    id: 3,
+                    message: 'Blabla',
+                    likesCount: 7,
+                    img: 'https://st4.depositphotos.com/5934840/25751/v/450/depositphotos_257517000-stock-' +
+                            'illustration-man-portrait-avatar.jpg'
+                }, {
+                    id: 4,
+                    message: 'da da',
+                    likesCount: 7,
+                    img: 'http://www.avatars24.de/img/avatars/avatar-5.jpg'
+                }
+            ],
+    
+            newPostText: 'agahuseynoff.com'
+        },
+    
+        dialogsPage: {
+            messages: [
+                {
+                    id: 1,
+                    message: 'Hi'
+                }, {
+                    id: 2,
+                    message: 'How are you'
+                }, {
+                    id: 3,
+                    message: 'Thank you..'
+                }, {
+                    id: 4,
+                    message: 'Thank you..'
+                }, {
+                    id: 5,
+                    message: 'Thank you..'
+                }, {
+                    id: 6,
+                    message: 'Thank you..'
+                }
+            ],
+    
+            dialogs: [
+                {
+                    id: 1,
+                    name: 'Dima'
+                }, {
+                    id: 2,
+                    name: 'Andrey'
+                }, {
+                    id: 3,
+                    name: 'Sveta'
+                }, {
+                    id: 4,
+                    name: 'Sasha'
+                }, {
+                    id: 5,
+                    name: 'Victor'
+                }, {
+                    id: 6,
+                    name: 'Victor'
+                }
+            ]
+        }
     },
 
-    dialogsPage: {
-        messages: [
-            {
-                id: 1,
-                message: 'Hi'
-            }, {
-                id: 2,
-                message: 'How are you'
-            }, {
-                id: 3,
-                message: 'Thank you..'
-            }, {
-                id: 4,
-                message: 'Thank you..'
-            }, {
-                id: 5,
-                message: 'Thank you..'
-            }, {
-                id: 6,
-                message: 'Thank you..'
-            }
-        ],
+    getState() {
+        return this._state;
+    },
 
-        dialogs: [
-            {
-                id: 1,
-                name: 'Dima'
-            }, {
-                id: 2,
-                name: 'Andrey'
-            }, {
-                id: 3,
-                name: 'Sveta'
-            }, {
-                id: 4,
-                name: 'Sasha'
-            }, {
-                id: 5,
-                name: 'Victor'
-            }, {
-                id: 6,
-                name: 'Victor'
-            }
-        ]
+    _callSubscriber () {
+        console.log('Add Post');
+    },
+
+     addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0,
+            img: 'http://www.luljettas.com/images/avatar/img-6.jpg'
+        };
+    
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber();
+    },
+
+    updateNewPostText (newText){
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+
+    subscribe (observer) {
+        this._callSubscriber = observer;
     }
 }
-
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0,
-        img: 'http://www.luljettas.com/images/avatar/img-6.jpg'
-    };
-
-    state.profilePage.posts.push(newPost);
-    rerenderEntireTree(state);
-};
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
+ 
 
 
-export default state;
+export default store;
+
+window.store = store;
